096d93b7c9c7ebb21f164eb29b97a0c40504eb4f
Jimkorna <72735841+Jimkorna@users.noreply.github.com>
2020-11-12 18:52:59 +0200

    Update squarestars.cpp

--- squarestars.cpp
+++ squarestars.cpp
@@ -1,29 +1,14 @@
 #include <stdio.h>
-
-int main(){
-	
-	int N;
-	printf("Insert N: "); scanf("%d" , &N);
-	int stars = N; 
-	int kena = N;
-	
-	for(int i = 0; i<stars; i++){
-	if(i == 0 || i == N-1)
-		for(int j=0; j<kena; j++) printf("*");
-	else
-	{
-		printf("*");
-		for(int h=0; h<N-2; h++)
-		printf(" ");
-		printf("*");
-		}	
-		
-	
-		
-		printf("\n");
-	} 
-	
-	
-	return 0;
+int main()
+{
+printf("**********");
+printf("\n*        *");
+printf("\n*        *");
+printf("\n*        *");
+printf("\n*        *");
+printf("\n*        *");
+printf("\n*        *");
+printf("\n**********");
+return 0;
 }
-//Sto parakatw programma o xrhsths eisagh mia timh N pou kathorizei tis pleures tou tetragonou//
+//To parakatw programma apla emfanizei to sxhma pou einai mesa stis printf//
